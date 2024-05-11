a17a44231b03d88cef0669a184476d088778f9b2
Abbos <abbosibrohimov07@gmail.com>
2024-05-11 16:23:02 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// JavaScript nima ?
+// Js faylni githubga bog'lash
+// JavaScriptni 3 xil ko'rinishda compile (yurgazib ko'rish) va ularning bir biridan farqi haqida
+// Nodejs o'rnatish
+
+
+
+console.log("Hello JavaScript separated")
