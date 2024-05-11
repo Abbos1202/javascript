3dab65d112fab24a321fc18a1808e7040e61eb3e
Abbos <abbosibrohimov07@gmail.com>
2024-05-11 16:25:59 +0300

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-// JavaScript nima ?
-// Js faylni githubga bog'lash
-// JavaScriptni 3 xil ko'rinishda compile (yurgazib ko'rish) va ularning bir biridan farqi haqida
-// Nodejs o'rnatish
+console.log("Hello JavaScript 1");
 
+let text =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi ipsam perferendis enim asperiores unde minus nostrum totam tempore reiciendis blanditiis rerum, deleniti eveniet, mollitia itaque quis error laudantium dolorem ducimus aut maiores molestiae! Reiciendis dolore pariatur nisi est sed consectetur iusto officia, dolorum cum tenetur? Ratione earum aut debitis repellat non, magnam, ducimus natus, iste numquam sint nostrum. Excepturi incidunt sapiente blanditiis ad repellendus earum dolorum, labore inventore recusandae hic vitae nihil veniam deserunt dolorem nam harum quis maiores vero quas alias laborum saepe! Quae, voluptatibus deserunt. Explicabo incidunt, totam corporis deleniti voluptate perferendis eligendi maiores facere dicta beatae fugit.";
 
-
-console.log("Hello JavaScript separated")
+  console.log(text);
